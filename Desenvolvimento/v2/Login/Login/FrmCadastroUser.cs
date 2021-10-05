df2237ccad0893be4e075a60bbb835334424facb
PaulaSena <senasilvaanapaula@gmail.com>
2021-10-05 14:10:56 -0300

    v2 Login

---
+++ Desenvolvimento/v2/Login/Login/FrmCadastroUser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class FrmCadastroUser : Form
+    {
+        public FrmCadastroUser()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCadastroUser_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void linkLblJaTenho_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmLogin frmLogin = new FrmLogin();
+            frmLogin.Show();
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            {
+                txtBoxUser.Text = "";
+                txtBoxSenha.Text = "";
+                txtBoxConfSenha.Text = "";
+            }
+        }
+
+        private void txtBoxConfSenha_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if
+            (txtBoxUser.Text == "" || txtBoxSenha.Text == "" && txtBoxConfSenha.Text == "")
+            {
+                MessageBox.Show("Por favor preencha todos os campos para cadastro de Usuario!");
+            }
+            else if (txtBoxSenha.Text == txtBoxConfSenha.Text)
+            {
+
+                MessageBox.Show("Usuario cadastrado com sucesso!");
+            }
+            else if (txtBoxSenha.Text != txtBoxConfSenha.Text)
+                {
+                MessageBox.Show("Senhas nao conferem!");
+                }
+        }
+        }
+    } 
+      
+
+
+    
+            
+            
+        
+    
---
+++ Desenvolvimento/v2/Login/Login/FrmLogin.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogarFunc_Click(object sender, EventArgs e)
+      
+            /* Se usuario validado verifique a senha*/
+        {
+            
+            /* Se Usuario e senha validada prossiga para a home*/
+
+            if (txtBoxSenha.Text == "adm" && txtBoxUser.Text == "adm")
+            {
+                Home home = new Home();
+                home.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario ou senha invalidos!");
+            }
+           
+           
+        }
+
+        private void lblInfo1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void lblInfo2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FrmLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void txtBoxUser_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void txtBoxSenha_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void txtBoxSenha_Enter(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtBoxSenha_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtBoxUser.Text = "";
+            txtBoxSenha.Text = "";
+        }
+
+        private void linkLblNaotenho_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmCadastroUser frmCadastroUser = new FrmCadastroUser();
+            frmCadastroUser.Show();
+        }
+    }
+}
